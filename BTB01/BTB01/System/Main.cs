108ab324eb702c0c071b9cc0862ccdd7e1be02aa
okabi <okabi>
2014-04-20 01:16:07 +0900

    Visual C#(2010)用のプロジェクトを立ち上げたのでアップ

---
+++ BTB01/BTB01/System/Main.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Main.cs
+ *   このクラスは、メイン制御を定義します。
+ *   実際に呼び出されるのはこのMain.Main()関数です。
+ *   
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DxLibDLL;
+
+namespace BTB01.System
+{
+    class main
+    {
+        static void Main()
+        {
+            // ウィンドウモードに切り替え
+            DX.ChangeWindowMode(DX.TRUE);
+
+            // DXライブラリの初期化に失敗したら終了
+            if (DX.DxLib_Init() == -1)
+            {
+                return;
+            }
+
+            DX.DrawString(100, 200, "ちんこｗ", DX.GetColor(255, 255, 255));
+
+            // キー入力待ち
+            DX.WaitKey();
+
+            // DXライブラリの終了
+            DX.DxLib_End();
+        }
+    }
+}
---
+++ BTB01/BTB01/System/Transition.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Transition.cs
+ *   このクラスは、画面遷移を定義します。
+ *   Scenesディレクトリ内に登録された画面から別の画面に切り替える制御を行います。
+ *   
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DxLibDLL;
+
+namespace BTB01.System
+{
+    class Transition
+    {
+    }
+}
